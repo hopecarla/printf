f38090383fd6509a035b4b106990742534c77167
hopecarla <hopememba.carla@gmail.com>
2023-10-20 06:08:52 -0700

    printf

--- printf.c
+++ printf.c
@@ -5,39 +5,48 @@
  * @...: Variable number of arguments to replace placeholders
  * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-    va_list args;
+	va_list args;
 
-    int printed_chars = 0;
-    int buffer_index = 0;
-    char buffer[BUFF_SIZE];
-    va_start(args, format);
+	int printed_chars = 0;
+	int buffer_index = 0;
+	char buffer[BUFF_SIZE];
 
+	va_start(args, format);
     while (*format)
     {
         if (*format != '%')
 	{
-            print_char(buffer, &buffer_index, *format) ? 0 : (write(1, buffer, buffer_index), buffer_index = 0);
-            printed_chars++;
-        } else
+            printed_chars += (print_char(buffer, &buffer_index, *format) ? 1 : 0);
+        }
+	else
 	{
             format++;
-            if (*format == 'c') print_char(buffer, &buffer_index, va_arg(args, int));
-            else if (*format == 's') printed_chars += print_string(buffer, &buffer_index, va_arg(args, const char *));
-            else if (*format == 'd') printed_chars += print_int(buffer, &buffer_index, va_arg(args, int));
-            else if (*format == '%') print_char(buffer, &buffer_index, '%');
-	    else 
+            switch (*format)
 	    {
-		     printed_chars += (print_char(buffer, &buffer_index, '%') ? 1 : 0);
-		     printed_chars += (print_char(buffer, &buffer_index, *format) ? 1 : 0);
-	    }
+                case 'c':
+                    printed_chars += (print_char(buffer, &buffer_index, va_arg(args, int)) ? 1 : 0);
+                    break;
+                case 's':
+                    printed_chars += print_string(buffer, &buffer_index, va_arg(args, const char *));
+                    break;
+                case 'd':
+                    printed_chars += print_int(buffer, &buffer_index, va_arg(args, int));
+                    break;
+                case '%':
+                    printed_chars += (print_char(buffer, &buffer_index, '%') ? 1 : 0);
+                    break;
+                default:
+                    printed_chars += (print_char(buffer, &buffer_index, '%') ? 1 : 0);
+                    print_percent(buffer, &buffer_index);
+                    printed_chars += (print_char(buffer, &buffer_index, *format) ? 1 : 0);
+            }
             format++;
         }
     }
 
     write(1, buffer, buffer_index);
     va_end(args);
-    return (printed_chars);
+    return printed_chars;
 }
